21c01f13ac38804159f917d0b1b23fc54a7b84d0
ChanDenBlue <146571822+ChanDenBlue@users.noreply.github.com>
2023-12-04 23:53:17 +0700

    Update Undo.java

--- Feature/Undo.java
+++ Feature/Undo.java
@@ -16,6 +16,7 @@ public class Undo {
     private Rectangle UndoButtonBound;
 
     private boolean isUndoUsed = false;
+
     public boolean isUndoUsed() {
         return isUndoUsed;
     }
@@ -24,98 +25,118 @@ public class Undo {
         isUndoUsed = undoUsed;
     }
 
-    public Undo(Playing playing){
+    public Undo(Playing playing) {
         this.playing = playing;
-        UndoButtonBound = new Rectangle(50,30,50,50);
+        UndoButtonBound = new Rectangle(50, 30, 50, 50);
     }
+
     private static Undo instance;
-    public static Undo createInstance(Playing playing){
-        if(instance == null){
+
+    public static Undo createInstance(Playing playing) {
+        if (instance == null) {
             instance = new Undo(playing);
             return instance;
         }
         return null;
     }
-    public void render(Graphics g){
+
+    public void render(Graphics g) {
         UndoButton = t.getImage(getClass().getResource("/resources/Undo.png"));
         UndoButtonUnavailable = t.getImage(getClass().getResource("/resources/undo_unavailable.png"));
-        g.drawImage(UndoButton,50,30,50,50,null);
+        g.drawImage(UndoButton, 50, 30, 50, 50, null);
         undoBlockedRender(g);
     }
-    public void storeCurrentEntities(){
+
+    public void storeCurrentEntities() {
         resetTile();
-        for(Entities entities:playing.getEntitiesManager().getEntitiesList()){
-            if(entities.isStatus()){
-                for(Tile tile1:playing.getBoard4x4().getTile()){
-                    if(tile1.isOccupied(entities)){
-                        tile1.setValueHold(entities.getValue());
+        for (Entities entities : playing.getEntitiesManager().getEntitiesList()) {
+            if (entities.isStatus()) {
+                Tile[][] tiles = playing.getBoard4x4().getTile();
+                for (Tile[] row : tiles) {
+                    for (Tile tile1 : row) {
+                        if (tile1.isOccupied(entities)) {
+                            tile1.setValueHold(entities.getValue());
+                        }
                     }
                 }
             }
         }
     }
-    public void resetTile(){
-        for(Tile tile:playing.getBoard4x4().getTile()){
-            tile.setValueHold(0);
+
+    public void resetTile() {
+        Tile[][] tiles = playing.getBoard4x4().getTile();
+        for (Tile[] row : tiles) {
+            for (Tile tile : row) {
+                tile.setValueHold(0);
+            }
         }
     }
-    public void UndoMove(int x, int y){
-        if(UndoButtonBound.contains(x,y)){
-            for(Entities entities:playing.getEntitiesManager().getEntitiesList()){
+
+    public void UndoMove(int x, int y) {
+        if (UndoButtonBound.contains(x, y)) {
+            for (Entities entities : playing.getEntitiesManager().getEntitiesList()) {
                 UndoStatus(entities);
             }
-//            checkQuantity();
-            for(Entities entities:playing.getEntitiesManager().getEntitiesList()){
+            // checkQuantity();
+            for (Entities entities : playing.getEntitiesManager().getEntitiesList()) {
                 UndoPosition(entities);
             }
             isUndoUsed = true;
         }
-//        checkQuantity();
+        // checkQuantity();
     }
-    public void UndoStatus(Entities entities){
-        if(entities.isEntitiesNewlyCreated()){
+
+    public void UndoStatus(Entities entities) {
+        if (entities.isEntitiesNewlyCreated()) {
             entities.setStatus(false);
             entities.setNum(0);
             entities.setEntitiesNewlyCreated(false);
         }
-        if(entities.isEntitiesNewlyDeleted()){
+        if (entities.isEntitiesNewlyDeleted()) {
             entities.setStatus(true);
             entities.setEntitiesNewlyDeleted(false);
             entities.setNum(0);
         }
-        if(entities.isEntitiesNewlyCombined()){
+        if (entities.isEntitiesNewlyCombined()) {
             entities.setStatus(false);
             entities.setEntitiesNewlyCombined(false);
             entities.setNum(0);
         }
     }
-    public void UndoPosition(Entities entities){
-        if(entities.isStatus()){
-            for(Tile tile:playing.getBoard4x4().getTile()){
-                if(tile.getValueHold() == entities.getValue()){
-                    entities.setPosition(tile);
-                    entities.setTileNum(tile.getTileNum());
-                    tile.setValueHold(0);
-                    break;
+
+    public void UndoPosition(Entities entities) {
+        if (entities.isStatus()) {
+            Tile[][] tiles = playing.getBoard4x4().getTile();
+            for (Tile[] row : tiles) {
+                for (Tile tile : row) {
+                    if (tile.getValueHold() == entities.getValue()) {
+                        entities.setPosition(tile);
+                        entities.setTileNum(tile.getTileNum());
+                        tile.setValueHold(0);
+                        break;
+                    }
                 }
             }
         }
     }
-    public void checkQuantity(){
+
+    public void checkQuantity() {
         int entities = 0;
-        for(Entities entities1:playing.getEntitiesManager().getEntitiesList()){
-            if(entities1.isStatus()){
+        for (Entities entities1 : playing.getEntitiesManager().getEntitiesList()) {
+            if (entities1.isStatus()) {
                 entities++;
             }
         }
         System.out.println(entities);
     }
-    public void undoBlockedRender(Graphics g){
-        if(isUndoUsed){
-            g.drawImage(UndoButtonUnavailable,50,30,50,50,null);
+
+    public void undoBlockedRender(Graphics g) {
+        if (isUndoUsed) {
+            g.drawImage(UndoButtonUnavailable, 50, 30, 50, 50, null);
         }
     }
-    public void update(){
-//        checkQuantity();
+
+    public void update() {
+        // checkQuantity();
     }
 }
