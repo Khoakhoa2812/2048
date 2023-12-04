a082909671871bb5a1e267e04f041b6a2ee15605
ChanDenBlue <146571822+ChanDenBlue@users.noreply.github.com>
2023-12-05 00:03:59 +0700

    Update Music.java

--- Audio/Music.java
+++ Audio/Music.java
@@ -12,23 +12,29 @@ public class Music {
     static {
         try {
             soundURL[0] = Music.class.getResource("/Audio/background.wav");
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Cannot open audio!"); //show error dialog
+            JOptionPane.showMessageDialog(null, "Cannot open audio!"); // show error dialog
         }
     }
-    public static void inputAudio(int i){
+
+    public static void inputAudio(int i) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundURL[i]);
             clips[i] = AudioSystem.getClip();
             clips[i].open(audioInputStream);
-        } catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
-    public static void backgroundMusic(){
-        inputAudio(0);
-        clips[0].start();
-        clips[0].loop(Clip.LOOP_CONTINUOUSLY);
+
+    public static void backgroundMusic() {
+        inputAudio(0); // Đảm bảo rằng audio đã được load trước khi phát
+        if (clips[0] != null) {
+            clips[0].start();
+            clips[0].loop(Clip.LOOP_CONTINUOUSLY);
+        } else {
+            System.out.println("Audio clip is null!"); // In ra thông báo lỗi nếu không load được audio
+        }
     }
 }
